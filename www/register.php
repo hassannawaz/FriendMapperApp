c6a75e75f718154b87ca6793dccb2ca8cb644c39
Syed Mutahir Hussain Kazmi <mutahir_kazmi@live.com>
2013-04-03 13:42:52 +0500

    Website Uploaded
    No Git Account Yet... Registration done using MYSQL LOCALHOST database.

---
+++ www/register.php
@@ -0,0 +1,21 @@
+<?php 
+// Prevent caching.
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 01 Jan 1996 00:00:00 GMT');
+
+$Name = $_POST['Name'];
+$Phone_Number = $_POST['PhoneNumber'];
+$Registered = $_POST['Registered'];
+$Visibility = $_POST['Visibility'];
+$Panic = $_POST['Panic'];
+$Latitude = $_POST['Latitude'];
+$Longitude = $_POST['Longitude'];
+
+ //Connect to mysql server""
+$con = mysqli_connect("localhost:3306", "root", "" ,"friendmapper" );
+	//Create query
+	$sql = "INSERT INTO friendmapper.members (Name, PhoneNumber, Registered, Visibility, Panic, Latitude, Longitude) VALUES ('".$Name."', '".$Phone_Number."', '".$Registered."', '".$Visibility."', '".$Panic."', '".$Latitude."', '".$Longitude."')";
+	
+mysqli_query($con,$sql);
+mysqli_close($con);
+?>
